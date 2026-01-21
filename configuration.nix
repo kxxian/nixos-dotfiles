ce9c70068575bbce9fc7310aea5e609dff3182d6
kxxian <kristianryanbulos@gmail.com>
2026-01-21 08:35:31 +0800

    changed perfomance profiles to power save in tlp conifg, to lower cpu temps

--- configuration.nix
+++ configuration.nix
@@ -107,10 +107,11 @@
     enable = true;
     settings = {
       # TLP_DEFAULT_MODE = "BAT";
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      # CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_AC = "powersave";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "powersave";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "powersave";
       START_CHARGE_THRESH_BAT0 = 40;
       STOP_CHARGE_THRESH_BAT0 = 80;
     };
