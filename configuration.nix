03a44293c65459f591ae6763a9acb986214e482b
kxxian <kristianryanbulos@gmail.com>
2026-02-18 08:50:55 +0800

    restored tlp

--- configuration.nix
+++ configuration.nix
@@ -122,26 +122,26 @@
 
   # services.auto-cpufreq.enable = true;
 
-  # services.tlp = {
-  #   enable = true;
-  #   settings = {
-  #
-  #     CPU_SCALING_GOVERNOR_ON_AC = "schedutil";
-  #     CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-  #
-  #     CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
-  #     CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-  #
-  #     CPU_BOOST_ON_AC = 1;
-  #     CPU_BOOST_ON_BAT = 0;
-  #
-  #     CPU_MAX_PERF_ON_AC = 100;
-  #     CPU_MAX_PERF_ON_BAT = 40;
-  #
-  #     START_CHARGE_THRESH_BAT0 = 40;
-  #     STOP_CHARGE_THRESH_BAT0 = 80;
-  #   };
-  # };
+  services.tlp = {
+    enable = true;
+    settings = {
+
+      CPU_SCALING_GOVERNOR_ON_AC = "schedutil";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
+
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MAX_PERF_ON_BAT = 40;
+
+      START_CHARGE_THRESH_BAT0 = 40;
+      STOP_CHARGE_THRESH_BAT0 = 80;
+    };
+  };
 
   services.openssh.enable = true;
 
