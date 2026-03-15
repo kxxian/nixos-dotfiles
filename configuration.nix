d371733ca0c6daed9fb2670317216628469c21dc
kxxian <kristianryanbulos@gmail.com>
2026-03-15 16:45:23 +0800

    weekly update

--- configuration.nix
+++ configuration.nix
@@ -50,12 +50,21 @@
 
   services.printing.enable = true;
 
+  services.pulseaudio.enable = false;
+
   hardware.bluetooth.enable = true;
+
+  security.rtkit.enable = true;
 
   services.pipewire = {
     enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
     pulse.enable = true;
+    jack.enable = true;
   };
+
+  services.pipewire.wireplumber.enable = true;
 
   services.libinput.enable = true;
 
